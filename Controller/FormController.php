e474daa3784922ee1bd6fc01a086175df16e63d1
Leny BERNARD <leny@appventus.com>
2015-09-20 16:56:21 +0200

    refactor to pass the widget id instead of every input hidden (please why !!) and fix variables replacement system

--- Controller/FormController.php
+++ Controller/FormController.php
@@ -2,11 +2,15 @@
 
 namespace Victoire\Widget\FormBundle\Controller;
 
+use Gedmo\Sluggable\Util\Urlizer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints\Email as EmailConstraint;
+use Victoire\Bundle\CoreBundle\Entity\Link;
+use Victoire\Bundle\MediaBundle\Entity\Media;
+use Victoire\Widget\FormBundle\Entity\WidgetForm;
 
 /**
  * FormController
@@ -30,9 +34,11 @@ class FormController extends Controller
         if ($request->getMethod() != "POST" && $request->getMethod() != "PUT") {
             throw $this->createNotFoundException();
         }
-        $taintedValues = $this->getRequest()->request->all()['cms_form_content'];
+        $_taintedValues = $this->getRequest()->request->all()['cms_form_content'];
+        /** @var WidgetForm $widget */
+        $widget = $this->get('doctrine.orm.entity_manager')->getRepository('VictoireWidgetFormBundle:WidgetForm')->find($_taintedValues['id']);
 
-        foreach ($taintedValues['questions'] as $question) {
+        foreach ($_taintedValues['questions'] as $question) {
             if (in_array($question['type'], array("text", "textarea")) && !empty($question[0])) {
                 $data[] =array(
                     'label' => $question["label"],
@@ -79,93 +85,101 @@ class FormController extends Controller
             }
         }
 
-        $isSpam = $this->testValues($taintedValues, $request);
+
+        ///////////////////////// SEND EMAIL TO ADMIN (set in the form or default one)  //////////////////////////////////////////
+
+        //$isSpam = $this->testForSpam($taintedValues, $request);
         $mailer = 'mailer';
-        $subject = $taintedValues['title'];
-        if (isset($taintedValues['targetEmail']) && !empty($taintedValues['targetEmail']) && !$isSpam) {
-            $targetEmail = !empty($taintedValues['targetEmail']) ? $taintedValues['targetEmail'] : $this->container->getParameter('victoire_widget_form.default_email_address');
-            if ($errors = $this->get('validator')->validateValue($taintedValues['targetEmail'], new EmailConstraint())) {
-                try {
-                    $to = $targetEmail;
-                    $from = array(
-                        $this->container->getParameter('victoire_widget_form.default_email_address') => $this->container->getParameter('victoire_widget_form.default_email_label'),
-                    );
-                    array_push($data, array('label' => 'ip', 'value' => $_SERVER['REMOTE_ADDR']));
-                    $body = $this->renderView(
-                        'VictoireWidgetFormBundle::managerMailTemplate.html.twig',
-                        array(
-                            'title' => $taintedValues['title'],
-                            'url' => $request->headers->get('referer'),
-                            'data' => $data,
-                        )
-                    );
-                    if(sizeof($regexErrors) == 0){
-                        $emailSend = true;
-                        $this->createAndSendMail($subject, $from, $to, $body, 'text/html', null, array(), $mailer);
-                    }
-                } catch (Exception $exc) {
-                    echo $exc->getTraceAsString();
+        $subject = $widget->getTitle();
+        $targetEmail = $widget->getTargetEmail() ? $widget->getTargetEmail() : $this->container->getParameter(
+            'victoire_widget_form.default_email_address'
+        );
+        if ($errors = $this->get('validator')->validateValue($widget->getTargetEmail(), new EmailConstraint())) {
+            try {
+                $from = array(
+                    $this->container->getParameter('victoire_widget_form.default_email_address') => $this->container->getParameter('victoire_widget_form.default_email_label'),
+                );
+                array_push($data, array('label' => 'ip', 'value' => $_SERVER['REMOTE_ADDR']));
+                $body = $this->renderView(
+                    'VictoireWidgetFormBundle::managerMailTemplate.html.twig',
+                    array(
+                        'title' => $widget->getTitle(),
+                        'url' => $request->headers->get('referer'),
+                        'data' => $data,
+                    )
+                );
+                if(sizeof($regexErrors) == 0){
+                    $emailSend = true;
+                    $this->createAndSendMail($subject, $from, $targetEmail, $body, 'text/html', null, array(), $mailer);
                 }
+            } catch (\Exception $e) {
+                echo $e->getTraceAsString();
             }
         }
+        ///////////////////////// AUTOANSWER (if email field exists and is filled properly)  //////////////////////////////////////////
         $email = null;
-        foreach ($taintedValues['questions'] as $question) {
+        foreach ($_taintedValues['questions'] as $question) {
             if ($question['label'] == "Email" || $question['label'] == "email") {
                 $email = $question[0];
             }
         }
-        if (!empty($taintedValues['autoAnswer']) && $taintedValues['autoAnswer'] == true && !empty($email) && !$isSpam) {
-            if ($errors = $this->get('validator')->validateValue($taintedValues['targetEmail'], new EmailConstraint())) {
+        if ($widget->isAutoAnswer() === true && $email) {
+            if ($errors = $this->get('validator')->validateValue($widget->getTargetEmail(), new EmailConstraint())) {
                 try {
-                    $body = $taintedValues['message'];
-                    preg_match_all("/{{.*?}}/", $body, $variables);
-                    foreach ($variables[0] as $variable) {
-                        if (!empty($taintedValues["questions"][$this->slugify($variable)])) {
-                            if (in_array($taintedValues["questions"][$this->slugify($variable)]['type'], array("text", "textarea")) && !empty($taintedValues["questions"][$this->slugify($variable)][0])) {
-                                $body = preg_replace("/$variable/", $taintedValues["questions"][$this->slugify($variable)][0], $body);
-                            } elseif ($taintedValues["questions"][$this->slugify($variable)]['type'] == "radio" && !empty($taintedValues["questions"][$this->slugify($variable)]["proposal"][0])) {
-                                $body = preg_replace("/$variable/", $taintedValues["questions"][$this->slugify($variable)]["proposal"][0], $body);
-                            } elseif ($taintedValues["questions"][$this->slugify($variable)]['type'] == "checkbox" && !empty($taintedValues["questions"][$this->slugify($variable)]["proposal"])) {
-                                $body = preg_replace("/$variable/", implode(', ', $taintedValues["questions"][$this->slugify($variable)]['proposal']), $body);
-                            } elseif ($taintedValues["questions"][$this->slugify($variable)]['type'] == "date" && !empty($taintedValues["questions"][$this->slugify($variable)]['Day']) && !empty($taintedValues["questions"][$this->slugify($variable)]['Month']) && !empty($taintedValues["questions"][$this->slugify($variable)]['Year'])) {
-                                $body = preg_replace("/$variable/", $taintedValues["questions"][$this->slugify($variable)]['Day']." ".$taintedValues["questions"][$this->slugify($variable)]['Month']." ".$taintedValues["questions"][$this->slugify($variable)]['Year'], $body);
+                    $urlizer = new Urlizer();
+                    $body = $widget->getMessage();
+                    preg_match_all("/{{(.*?)}}/", $body, $variables);
+                    foreach ($variables[1] as $index => $variable) {
+                        $pattern = "/".$variables[0][$index]."/";
+                        foreach ($_taintedValues["questions"] as $_question) {
+                            //Allow exact and urlized term (ex: for a field named Prénom => prenom, Prénom, Prenom are ok)
+                            if ($_question['label'] === $variable || $urlizer->urlize($_question['label']) === $urlizer->urlize($variable)) {
+                                switch ($_question['type']) {
+                                    case 'radio':
+                                        $body = preg_replace($pattern, $_question["proposal"][0], $body);
+                                        break;
+                                    case 'checkbox':
+                                        $body = preg_replace($pattern, implode(', ', $_question["proposal"]), $body);
+                                        break;
+                                    case 'date':
+                                        $body = preg_replace($pattern, $_question['Day']." ".$_question['Month']." ".$_question['Year'], $body);
+                                        break;
+                                    default: //text, textarea
+                                        $replacement = $_question[0];
+                                        $body = preg_replace($pattern, $replacement, $body);
+                                }
                             }
-                            $body = preg_replace("/$variable/", "", $body);
                         }
+
+                        //If we didn't found the variable in any field, we cleanup by removing the variable in the body to not appear like buggy to the final user
+                        $body = preg_replace($pattern, "", $body);
                     }
                     //Send an email to the customer AND to the specified email target
-                    $to = $email;
-                    if ($this->container->getParameter('victoire_widget_form.default_bcc_email_address', null)) {
-                        $replyTo = ($this->container->getParameter('victoire_widget_form.default_bcc_email_address'));
-                    }
-
                     $from = array(
                         $this->container->getParameter('victoire_widget_form.default_email_address') => $this->container->getParameter('victoire_widget_form.default_email_label'),
                     );
 
-                    $subject = ($taintedValues['subject']);
                     $body = $this->renderView(
                         'VictoireWidgetFormBundle::customerMailTemplate.html.twig',
                         array(
                             'message' => $body,
                         )
                     );
-                    $em = $this->getDoctrine()->getManager();
-                    $mediaRepo = $em->getRepository('\Victoire\Bundle\MediaBundle\Entity\Media');
                     $attachments = array();
                     foreach (array('attachmentUrl', 'attachmentUrl2', 'attachmentUrl3', 'attachmentUrl4', 'attachmentUrl5', 'attachmentUrl6', 'attachmentUrl7') as $field) {
-                        if (!empty($taintedValues[$field])) {
-                            $file = $mediaRepo->findOneById($taintedValues[$field]);
-                            $filePath = $this->container->getParameter('kernel.root_dir').'/../web'.$file->getUrl();
-                            $file = new UploadedFile($filePath, $file->getName());
-                            $attachments[] =  $file;
+                        $getAttachment = 'get'.ucfirst($field);
+                        /** @var Media $attachment */
+                        if ($attachment = $widget->$getAttachment()) {
+                            $filePath = $this->container->getParameter('kernel.root_dir').'/../web'.$attachment->getUrl();
+                            $attachment = new UploadedFile($filePath, $attachment->getName());
+                            $attachments[] =  $attachment;
                         }
                     }
                     if(sizeof($regexErrors) == 0){
                         $emailSend = true;
-                        $this->createAndSendMail($subject, $from, $to, $body, 'text/html', null, $attachments, $mailer);
+                        $this->createAndSendMail($widget->getSubject(), $from, $email, $body, 'text/html', $widget->getTargetemail(), $attachments, $mailer);
                     }
-                } catch (Exception $exc) {
+                } catch (\Exception $exc) {
                     echo $exc->getTraceAsString();
                 }
             }
@@ -194,7 +208,7 @@ class FormController extends Controller
             if ($error != '') {
                 $this->container->get('appventus_alertifybundle.helper.alertifyhelper')->scold($error);
             }
-       }
+        }
         $redirectUrl = $redirectUrl ?: $request->headers->get('referer');
 
         return $this->redirect($redirectUrl);

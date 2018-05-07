2bea5d05954d842ac0370785ec7c63a4f72c53b5
Matteo Peronnet <matteo-peronnet@hotmail.fr>
2018-05-07 14:24:51 +0000

    Adding captcha by ip with iphub

---
+++ Domain/Captcha/Adapter/IpHubAdapter.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Victoire\Widget\FormBundle\Domain\Captcha\Adapter;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class IpHubAdapter implements CaptchaInterface {
+
+    private $iphubPrivateKey;
+    private $requestStack;
+
+    public function __construct($iphubPrivateKey, RequestStack $requestStack)
+    {
+        $this->iphubPrivateKey = $iphubPrivateKey;
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * Check if the captcha is valid or not
+     * @return boolean
+     */
+    public function validateCaptcha()
+    {
+        $request = $this->requestStack->getCurrentRequest();
+        return !$this->isBadIP($request->getClientIp(), $this->iphubPrivateKey);
+    }
+
+    /**
+     * Get the captcha name
+     * @return string
+     */
+    public function getName()
+    {
+        return 'iphub';
+    }
+
+    /**
+     * Check if current configuration allow to use this captcha
+     * @return boolean
+     */
+    public function canBeUsed()
+    {
+        return $this->iphubPrivateKey;
+    }
+
+    /**
+     * Return all parameters necessary in the view
+     * @return array
+     */
+    public function getTwigParameters()
+    {
+        return [];
+    }
+
+    private function isBadIP($ip, $key, $strict = false) {
+        $ch = curl_init();
+        curl_setopt_array($ch, [
+            CURLOPT_URL => "http://v2.api.iphub.info/ip/{$ip}",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => ["X-Key: {$key}"]
+        ]);
+
+        try {
+            $block = json_decode(curl_exec($ch))->block;
+        } catch (Exception $e) {
+            throw $e;
+        }
+        if ($block) {
+            if ($strict) {
+                return true;
+            } elseif (!$strict && $block === 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}

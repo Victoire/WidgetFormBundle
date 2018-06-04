43f8253b853d77a021affdba98c3086ae0695145
Matteo Peronnet <matteo-peronnet@hotmail.fr>
2018-06-04 14:47:04 +0200

    Update dependencies.sh

--- Tests/dependencies.sh
+++ Tests/dependencies.sh
@@ -12,12 +12,10 @@ echo ">>> Update Behat config"
 
 # Get the Namespace of EmailContext file with regular expression
 # Alter the namespace to remove ';' then use triple backslashes instead of a single backslash
-namespace="$(cat ../../../Context/EmailContext.php | sed -rn 's/namespace ((\\{1,2}\w+|\w+\\{1,2})(\w+\\{0,2})+)/\1/p' | sed -r 's/;+$//' | sed -e 's|\\|\\\\\\|g' )"
+namespace="$(cat ../../../Tests/Context/EmailContext.php | sed -rn 's/namespace ((\\{1,2}\w+|\w+\\{1,2})(\w+\\{0,2})+)/\1/p' | sed -r 's/;+$//' | sed -e 's|\\|\\\\\\|g' )"
 # Add EmailContext path in the behat.yml.dist file to load the context
 sed -i "s@contexts:@contexts: \n                 - $namespace\\\EmailContext@" behat.yml.dist
 rm ../../../Tests/Context/EmailContext.php
-sed -i "s@extensions:@extensions: \n         Alex\\\MailCatcher\\\Behat\\\MailCatcherExtension\\\Extension:\n            url: http://fr.victoir
-e.io:1080\n            purge_before_scenario: true@" behat.yml.dist
+sed -i "s@extensions:@extensions: \n         Alex\\\MailCatcher\\\Behat\\\MailCatcherExtension\\\Extension:\n            url: http://fr.victoire.io:1080\n            purge_before_scenario: true@" behat.yml.dist
 # Use Chrome instead of firefox
 sed -i -e 's/firefox/chrome/g' behat.yml.dist
-
